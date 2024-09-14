ed3bcf553aad74e910a8bafbdb3aa44005420dad
thomas-nguyen1701 <121322450+thomas-nguyen1701@users.noreply.github.com>
2024-09-14 15:22:37 -0700

    'commited changes on training model'

--- main.py
+++ main.py
@@ -5,5 +5,17 @@ import seaborn as sns
 
 
 data = pd.read_csv("California_Houses.csv")
+
+data.dropna(inplace=True)
+
 data.info()
 
+from sklearn.model_selection import train_test_split
+
+x = data.drop(['Median_House_Value']) #training model
+y = data['Median_House_Value']
+x_train, x_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
+
+
+train_data = x_train.join(y_train)
+train_data.hist()
