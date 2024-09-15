5dd1e8083e32525e44a3e5da4c74ae03f9bb21aa
Thomas Nguyen <thomas.nguyen1701@gmail.com>
2024-09-14 19:58:03 -0700

    another update :D

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ data = pd.read_csv("California_Houses.csv")
 
 data.dropna(inplace=True)
 
-data.info()
-
 from sklearn.model_selection import train_test_split
 
-x = data.drop(['Median_House_Value']) #training model
+x = data.drop(['Median_House_Value'], axis=1) #training model
 y = data['Median_House_Value']
-x_train, x_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
 
 
 train_data = x_train.join(y_train)
-train_data.hist()
+train_data.hist(figsize=(15,8))
