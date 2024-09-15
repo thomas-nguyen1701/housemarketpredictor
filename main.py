d23716ebfb2f5015354fb70e128193f9a4420674
Thomas Nguyen <thomas.nguyen1701@gmail.com>
2024-09-15 13:54:23 -0700

    changes to main added heat map

--- main.py
+++ main.py
@@ -16,4 +16,10 @@ x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
 
 
 train_data = x_train.join(y_train)
-train_data.hist(figsize=(15,8))
+train_data = hst(figsize=(15,8))
+
+plt.figure(figsize=(15,8))
+sns.heatmap(train_data.corr(), annot=True, cmap="YlGnBu")
+
+
+
